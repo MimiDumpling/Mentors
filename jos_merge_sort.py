c9753a75d5151f32a9486ff30344089b9765861a
MimiDumpling <tienmiminguyen@gmail.com>
2017-09-25 22:14:03 +0000

    started merge sort for Jo

---
+++ jos_merge_sort.py
@@ -0,0 +1,32 @@
+""" Implement merge sort. 
+Merge sort takes two lists (sorted or unsorted), merges
+the lists and sorts them.
+
+Examples:
+
+    >>> merge_sort([1, 3, 5], [2, 4, 6])
+    [1, 2, 3, 4, 5, 6]
+
+    >>> merge_sort([3, 1, 5], [4, 2, 6])
+    [1, 2, 3, 4, 5, 6]
+
+    >>> merge_sort([1, 2, 4, 7], [3, 5, 6, 8])
+    [1, 2, 3, 4, 5, 6, 7, 8]
+
+"""
+
+def merge_sort(lst):
+    """Merge sort list and return result."""
+
+def make_merge(lst1, lst2):
+    """Merge two lists."""
+
+
+
+
+
+if __name__ == '__main__':
+    import doctest
+
+    if doctest.testmod().failed == 0:
+        print "\n*** ALL TEST PASSED. AWESOMESAUCE!\n"
