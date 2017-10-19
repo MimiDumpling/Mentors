df92909bbfbac2a3850ce960fe31bef40c08f107
MimiDumpling <tienmiminguyen@gmail.com>
2017-10-19 21:00:59 +0000

    starting recursively reversing singly linked list

---
+++ jos_recursive_reverse_singly_ll.py
@@ -0,0 +1,53 @@
+class Node(object):
+
+    def __init__(self, data, next):
+        self.data = data
+        self.next = next
+
+    def getData(self):
+        return self.data
+
+    def getNext(self):
+        return self.next
+
+    def setData(self, newdata):
+        self.data = newdata
+
+    def setNext(self, newnext):
+        self.next = newnext
+
+
+class SingleList(object):
+
+    head = None
+
+    def append(self, data):
+        new_node = Node(data, None)
+        if self.head is None:
+            self.head = new_node
+        else:
+            cur = self.head
+            while cur.next:
+                cur = cur.next
+            cur.next = new_node
+
+    def show(self):
+        print "Show list data:"
+        current_node = self.head
+        while current_node is not None:
+            print current_node.data
+            current_node = current_node.next
+
+def reverse(sll, current_node):
+    # Magic happens here
+    # sll is only passed in so that you can update head when you hit the base case
+
+
+if __name__ == '__main__':
+    s = SingleList()
+    s.append(1)
+    s.append(2)
+    s.append(3)
+    s.show()
+    reverse(s, s.head)
+    s.show()
