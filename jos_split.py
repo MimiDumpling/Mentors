7988d0ccd3b9165ad40397a4bd309f572adbef89
MimiDumpling <tienmiminguyen@gmail.com>
2017-12-05 14:47:16 -0800

    started split function

---
+++ jos_split.py
@@ -0,0 +1,26 @@
+"""
+Implement .split() without using .split().
+
+Edge cases to keep in mind:
+ 1) " " is not ""
+ 2) "hey    you" (multiple spaces)
+    ["hey", "you"]
+ 3) "sup" (end and see no char)
+    ["sup"]
+"""
+
+def split_me (char, string):
+
+    result = []
+
+    last_seen = -1
+
+    for index, x in enumerate(string):
+        if x == char:
+            result.append(string[last_seen + 1 : index])
+
+            last_seen = index
+
+        result.append(string[last_seen: ])
+
+    return result
