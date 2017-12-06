669c243ec0dd478fdbd7c85a077a4118a5b05e6c
MimiDumpling <tienmiminguyen@gmail.com>
2017-12-06 14:05:11 -0800

    deleted random new lines and raised ValueError

--- jos_split.py
+++ jos_split.py
@@ -10,20 +10,15 @@ Edge cases to keep in mind:
 """
 
 def split_me(char, string):
-
     result = []
-
     last_seen = -1
 
     if char == "":
-        return "Sorry, doesn't work if char is an empty string."
+        raise ValueError("Split argument must not be empty.")
 
     for index, x in enumerate(string):
-
         if x == char and last_seen + 1 != index:
-
             result.append(string[(last_seen + 1):index])
-
             last_seen = index
 
     if result == []:
@@ -31,7 +26,6 @@ def split_me(char, string):
         return result
     else:
         result.append(string[last_seen + 1:])
-
         return result
 
 print "Test 1: ", split_me(" ", "")
@@ -39,8 +33,8 @@ print "Test 2: ", split_me(" ", "hey")
 print "Test 3: ", split_me(" ", "hey there")
 print "Test 4: ", split_me(" ", "hey there, you")
 print "Test 5: ", split_me(" ", "hey       there")
-print "Test 6: ", split_me("", "hey       there")
-print "Test 7: ", split_me("e", "hey there")
+print "Test 6: ", split_me("e", "hey there")
+print "Test 7: ", split_me("", "hey       there")
 
 
 
